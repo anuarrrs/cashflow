e90f53793cc26c9a8a959668b20ecefb43dabe38
anuarrrs <bayanov.2002@gmail.com>
2026-03-31 21:42:54 +0500

    i'm dumb
    Replaced environment variables with hardcoded Supabase URL and anon key.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://xqvtnjtqbvgtufqpsldk.supabase.co'//import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = 'sb_publishable_8vaTbDoLzDztNL0WsKrmOg_ifdEsw5b' //import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
